adb1cb1c67dbe55e37cc8fba36db73efa079473d
janabras <jan.abras01@estudiant.upf.edu.com>
2023-02-24 14:29:20 +0100

    Replacing scanner.c

--- src/scanner.c
+++ src/scanner.c
@@ -1,17 +1,24 @@
-
-
 #include "scanner.h"
 #include "utilsFiles.h"
 #include <unistd.h>
 #include <fcntl.h>
 
+#define DEBUG 1 // or 0 to turn off debugging
+
+
 void outputMessage(char *outFileName)
 {
-
-    printf("\n");
-    printf("-------------------------------------------------------------\n");
-    printf("-------- scanned file saved in %s --------\n", outFileName);
-    printf("-------------------------------------------------------------\n");
+    if (DEBUG == 1) {
+        printf("\n");
+        printf("-------------------------------------------------------------\n");
+        printf("-------- scanned file saved in %s --------\n", outFileName);
+        printf("-------------------------------------------------------------\n");
+    } else {
+        printf("\n");
+        printf("-------------------------------------------------------------\n");
+        printf("-------- scanned file saved in stdout --------\n");
+        printf("-------------------------------------------------------------\n");
+    }
 }
 
 void flagHelp()
@@ -30,7 +37,13 @@ int main(int argc, char const *argv[]) {
     // Open files
     FILE *input_file = openFile(input_file_name, "r");
     if (input_file == NULL) return 1;
-    FILE *output_file = openFile(output_file_name, "w");
+
+    FILE *output_file;
+    if (DEBUG == 1) {
+        output_file = openFile(output_file_name, "w");
+    } else {
+        output_file = stdout;
+    }
 
     // process function
     process(input_file, output_file);
